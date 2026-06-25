8286de741a61403ac83ae8934958113460f92a86
slayerrider0097-byte <slayerrider0097@gmail.com>
2026-06-26 05:08:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,8 +43,8 @@ class Config:
         self.SESSION3: str = getenv("STRING_SESSION3", "")
 
         # Support Links
-        self.SUPPORT_CHANNEL: str = getenv("SUPPORT_CHANNEL", "https://t.me/loveguruoo")
-        self.SUPPORT_CHAT: str = getenv("SUPPORT_CHAT", "https://t.me/+571PcuJAHeU0MTU1")
+        self.SUPPORT_CHANNEL: str = getenv("SUPPORT_CHANNEL", "https://t.me/ArushUpdates")
+        self.SUPPORT_CHAT: str = getenv("SUPPORT_CHAT", "https://t.me/ArushSupport")
 
         # Excluded Chats
         self.EXCLUDED_CHATS: List[int] = self._parse_excluded_chats()
